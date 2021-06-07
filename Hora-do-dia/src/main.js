3f3f6d25b16415960bf31f82d24ffa4c9b5cffdf
Carla Bruckmann <72099071+CarlaBruckmann@users.noreply.github.com>
2021-06-06 21:56:38 -0300

    refactor(project/index) remove msg and src

--- Hora-do-dia/src/main.js
+++ Hora-do-dia/src/main.js
@@ -3,7 +3,7 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-
+    
 
     if(hora >= 0 && hora < 12){
         img.src = 'assets/img/foto-manha.png'
