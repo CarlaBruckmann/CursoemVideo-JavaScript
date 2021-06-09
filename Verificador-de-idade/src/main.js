53ab59312d8bf64980b511bb862fa2700f906da9
Carla Bruckmann <72099071+CarlaBruckmann@users.noreply.github.com>
2021-06-08 21:44:20 -0300

    feat(project) initial commit
    Added index, style and js.

---
+++ Verificador-de-idade/src/main.js
@@ -0,0 +1,45 @@
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var formAno = window.document.getElementById('txtano')
+   var res = window.document.querySelector('div#res')
+
+   if(formAno.value.length === 0 || Number(formAno.value) > ano){
+        window.alert('[ERRO]Verifique os dados e tente novamente!')
+   }else{
+       var fsex = window.document.getElementsByName('radsex')
+       var idade = ano - Number(formAno.value)
+       var gênero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id','foto')
+
+       if (fsex[0].checked){
+           gênero = 'Mulher'
+           if(idade >= 0 && idade < 10){
+               img.setAttribute('src','assets/img/criança-F.png')
+           }else if(idade < 21){
+            img.setAttribute('src','assets/img/adolescente-F.png')
+           }else if(idade < 50){
+            img.setAttribute('src','assets/img/adulto-F.png')
+           }else{
+            img.setAttribute('src','assets/img/idoso-F.png')
+           }
+       }else if(fsex[1].checked){
+           gênero = 'Homem'
+           if(idade >= 0 && idade < 10){
+            img.setAttribute('src','assets/img/criança-M.png')
+           }else if(idade < 21){
+            img.setAttribute('src','assets/img/adolescente-M.png')
+           }else if(idade < 50){
+            img.setAttribute('src','assets/img/adulto-M.png')
+           }else{
+            img.setAttribute('src','assets/img/idoso-M.png')
+           }
+       }
+       res.style.textAlign = 'center'
+       res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+       res.appendChild(img)
+   }
+
+
+}
