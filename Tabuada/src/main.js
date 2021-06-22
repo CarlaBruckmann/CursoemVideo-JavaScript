506a9f022c047038c13a30d0a7f55d8625c1c182
Carla Bruckmann <72099071+CarlaBruckmann@users.noreply.github.com>
2021-06-22 17:26:28 -0300

    refactor(app) added links/ icons
    Added links/ icons

--- Tabuada/src/main.js
+++ Tabuada/src/main.js
@@ -1,12 +1,11 @@
-//Houdini Background
 (async function () {
     if (CSS["paintWorklet"] === undefined)
-        await import("https://unpkg.com/css-paint-polyfill")
-
+      await import("https://unpkg.com/css-paint-polyfill");
     CSS.paintWorklet.addModule(
-        "https://rawcdn.githack.com/CSSHoudini/css-houdini/6979b873e80f9120f52bd481fbdf2d4c60db6b19/src/connections/dist/connections.js"
-    )
-})()
+      "https://rawcdn.githack.com/CSSHoudini/css-houdini/6979b873e80f9120f52bd481fbdf2d4c60db6b19/src/connections/dist/connections.js"
+    );
+  })();
+
 function multiplicar() {
     let num = window.document.getElementById('txtn')
     let tab = window.document.getElementById('seltab')
@@ -14,7 +13,7 @@ function multiplicar() {
     if (num.value.length == 0) {
         window.alert('Por favor, digite um número!')
     } else {
-        let n = Number(num.value) //numero 
+        let n = Number(num.value) 
         tab.innerHTML = ''
         for(let c = 1;c <= 10;c++){
             let item = document.createElement('option')
