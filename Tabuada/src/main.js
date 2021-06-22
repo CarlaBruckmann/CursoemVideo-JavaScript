5c39902111ecdee9d6252a00543ac6a761f5bce2
Carla Bruckmann <72099071+CarlaBruckmann@users.noreply.github.com>
2021-06-22 16:22:58 -0300

    feat (project) added index, style and js
    Added index, style and js - Tabuada.

---
+++ Tabuada/src/main.js
@@ -0,0 +1,26 @@
+//Houdini Background
+(async function () {
+    if (CSS["paintWorklet"] === undefined)
+        await import("https://unpkg.com/css-paint-polyfill")
+
+    CSS.paintWorklet.addModule(
+        "https://rawcdn.githack.com/CSSHoudini/css-houdini/6979b873e80f9120f52bd481fbdf2d4c60db6b19/src/connections/dist/connections.js"
+    )
+})()
+function multiplicar() {
+    let num = window.document.getElementById('txtn')
+    let tab = window.document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value) //numero 
+        tab.innerHTML = ''
+        for(let c = 1;c <= 10;c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
